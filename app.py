794ad6845d49cb7051e61607f56700775847386a
mohanapriyavakkalagadda <143479952+mohanapriyavakkalagadda@users.noreply.github.com>
2024-02-21 10:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import streamlit as st
 import os
 import pandas as pd
 import joblib as jb
-heading_style = '''
-<div style="color:red;" align='center'>
-<h1>Heart Stroke Prediction</h1>
-</div>
-'''
+st.markdown(f'''
+<h1 align='center'>Heart Stroke Prediction</h1>'''
 def return_df(gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status):
     kbn={
 	'gender':[gender],
